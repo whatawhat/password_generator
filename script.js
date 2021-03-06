f5269c7f43319c987e74e21d654996f0b677d239
Stephanie <whatawhat@hotmail.com>
2021-03-05 19:41:14 -0500

    added an array and function

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function critSelect() {
-  var generateBtn = prompt("Please select the criteria you need for your password.");
-  alert (generateBtn);
-}
 
-generateBtn.addEventListener("click", critSelect);
 
 // Write password to the #password input
 function writePassword() {
@@ -19,3 +14,35 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+//My work sofar
+
+
+//What I added
+function critSelect() {
+  var generateBtn = prompt("Please select the criteria you need for your password.");
+  alert (generateBtn);
+}
+
+generateBtn.addEventListener("click", critSelect);
+
+
+
+//created an array
+let critOptions = ["lowercase", "uppercase", "numeric", "special symbol"];
+
+
+//create a function - what I did
+var length = "";
+function passLength() {
+var length = prompt("Enter how many characters yourpassword needs to be");
+if (length < 8) {
+  "Number has to be at least 8";
+}
+else if (length > 128)
+  "Number has to be less than 129";
+} else {
+  "Thank you!";
+}
