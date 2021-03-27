4ff7ab78b4bea0f45253e9b7bb0c960ca2f63642
Stephanie <whatawhat@hotmail.com>
2021-03-27 17:06:16 -0400

    Added variables so at least one selected criteria is in the password

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+console.log(passwordText);
   passwordText.value = password;
 
 }
@@ -20,23 +20,53 @@ generateBtn.addEventListener("click", writePassword);
 //My work so far
 function generatePassword() {
   var length = prompt("Enter how many characters your password needs to be");
-  while (length < 8 || length > 128) {
-    alert("Number has to be between 8 and 128 characters");
-    length = prompt("Enter how many characters your password needs to be");
+  if (length < 8 || length > 128) {
+    alert("Number has to be between 8 and 128 characters. Please click button again.");
+    return;
   }
 
-  var uppercase = prompt("Do you want uppercase letters?");
-  var lowercase = prompt("Do you want lowercase letters?");
-  var digits = prompt("Do you want any digits?");
-  var special = prompt("Do you want any special symbols?");
+  var uppercase = confirm("Do you want uppercase letters?");
+  var lowercase = confirm("Do you want lowercase letters?");
+  var digits = confirm("Do you want any digits?");
+  var special = confirm("Do you want any special symbols?");
+console.log(uppercase);
 
-  while (uppercase === "n" && lowercase === "n" && digits === "n" && special === "n") {
-    alert("Please select at least one type of character.");
-    uppercase = prompt("Do you want uppercase letters?");
-    lowercase = prompt("Do you want lowercase letters?");
-    digits = prompt("Do you want any digits?");
-    special = prompt("Do you want any special symbols?");
+  // while (uppercase === "n" && lowercase === "n" && digits === "n" && special === "n") {
+  //   alert("Please select at least one type of character.");
+  //   uppercase = prompt("Do you want uppercase letters?");
+  //   lowercase = prompt("Do you want lowercase letters?");
+  //   digits = prompt("Do you want any digits?");
+  //   special = prompt("Do you want any special symbols?");
+  // }
 
+  var password = "";
+  var allChar = "";
+  if (uppercase) {
+    var uppercaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    password += uppercaseString[Math.floor(Math.random()*uppercaseString.length)]
+    allChar += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
+  if (lowercase) {
+    var lowercaseString = "abcdefghijklmnopqrstuvwxyz"
+    password += lowercaseString[Math.floor(Math.random()*lowercaseString.length)]
+    allChar += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (digits) {
+    var digitsString = "1234567890"
+    password += digitsString[Math.floor(Math.random()*digitsString.length)]
+    allChar += "1234567890";
+  }
+  if (special) {
+    var specialString = "!@#$%^&*()\"\'+=,-./;:<>?[]_\\`~{}|"
+    password += specialString[Math.floor(Math.random()*specialString.length)]
+    allChar += "!@#$%^&*()\"\'+=,-./;:<>?[]_\\`~{}|";
+  }
+
+  console.log("This is my message before the for loop " + password);
+
+  for (var i=password.length; i<length; i++) {
+    password += allChar[Math.floor(Math.random()*allChar.length)];
+  }
+  return password;
 }
 
